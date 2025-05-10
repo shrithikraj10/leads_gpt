fdfb7506b7bb86c67e4f5422b0091451a32f59fb
Shrithik Raj <lio.srk10@gmail.com>
2025-05-10 19:43:39 +0530

    Added a test api route

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api import routes_upload, routes_generate
+from app.api import routes_upload, routes_generate, routes_test
 from app.core.config import settings
 
 """Base of operations: The main.py to fulfill all the routing of different API endpoints/ Pages"""
@@ -13,6 +13,7 @@ app = FastAPI(
 # Include routes
 app.include_router(routes_upload.router, prefix="/upload", tags=["Upload"])
 app.include_router(routes_generate.router, prefix="/generate", tags=["Generate"])
+app.include_router(routes_test.router)
 
 @app.get("/")
 def read_root():
